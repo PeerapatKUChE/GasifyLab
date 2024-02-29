505e1e0b2d0a339c4dd20373f10556c04c0544ce
peerapatjaiyen <161545115+peerapatjaiyen@users.noreply.github.com>
2024-02-29 16:59:20 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,3 +4,14 @@ st.title("Predict H₂ & CO₂ from Biomass Gasification")
 st.text("Predict H₂ & CO₂ from Biomass Gasification")
 
 particle_size = st.text_input('Particle size (mm)')
+carbon = st.text_input('Carbon (%daf)')
+hydrogen = st.text_input('Hydrogen (%daf)')
+ash = st.text_input('Carbon (%db)')
+moisture = st.text_input('Moisture (%wb)')
+
+temperature = st.text_input('Temperature (°C)')
+steam_biomass = st.text_input('Steam/biomass ratio (wt/wt)')
+equivalence_ratio = st.text_input('Equivalence ratio of non-steam agent')
+
+feedstock = st.selectbox('Feedstock', ('Herbaceous biomass', 'Municipal solid waste', 'Sewage sludge', 'Woody biomass', 'Other'))
+
