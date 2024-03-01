3bb1351d075728af5c8c0e620b9157c7b92bc231
peerapatjaiyen <161545115+peerapatjaiyen@users.noreply.github.com>
2024-03-01 14:29:24 +0700

    Update main.py

--- main.py
+++ main.py
@@ -85,6 +85,7 @@ if not any(value is None for value in categorical_inputs.values()):
         encoded_categorical_vars = pd.concat([encoded_categorical_vars, encoded_categorical_input], axis=1)
 
 if not any(value == "" for value in continuous_inputs.values()):
+    continuous_inputs = float(continuous_inputs)
     normalized_continuous_vars = normalize(x=pd.DataFrame(continuous_inputs, index=[0]), x_original=pd.DataFrame(continuous_vars))
 
     st.dataframe(normalized_continuous_vars)
