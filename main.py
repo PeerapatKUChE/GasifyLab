5a26c0b8c589e84153271dca18d42ab912cf2112
peerapatjaiyen <161545115+peerapatjaiyen@users.noreply.github.com>
2024-03-01 21:02:27 +0700

    Update main.py

--- main.py
+++ main.py
@@ -52,22 +52,23 @@ st.title("Biomass Gasification Product Prediction Tool")
 st.text("All fields are required.")
 
 with st.container(border=True):
-    particle_size = st.number_input("Particle size (mm)", value=None, min_value=0.00)
+    particle_size = st.number_input("Particle size (mm)", value=None, min_value=0.00, key="Particle size")
     carbon, hydrogen = st.columns(2)
     ash, moisture = st.columns(2)
-    temperature = st.number_input("Temperature (°C)", value=None, min_value=0.00)
+    temperature = st.number_input("Temperature (°C)", value=None, min_value=0.00, key="Temperature")
     steam_biomass, equivalence_ratio = st.columns(2)
 
     continuous_inputs = {
         "Particle size": particle_size,
-        "C": carbon.number_input("Carbon (%daf)", value=None, min_value=0.00, max_value=100.00),
-        "H": hydrogen.number_input("Hydrogen (%daf)", value=None, min_value=0.00, max_value=100.0),
-        "Ash": ash.number_input("Ash (%db)", value=None, min_value=0.00, max_value=100.00),
-        "Moisture": moisture.number_input("Moisture (%wb)", value=None, min_value=0.00, max_value=100.00),
+        "C": carbon.number_input("Carbon (%daf)", value=None, min_value=0.00, max_value=100.00, key="C"),
+        "H": hydrogen.number_input("Hydrogen (%daf)", value=None, min_value=0.00, max_value=100.0, key="H"),
+        "Ash": ash.number_input("Ash (%db)", value=None, min_value=0.00, max_value=100.00, key="Ash"),
+        "Moisture": moisture.number_input("Moisture (%wb)", value=None, min_value=0.00, max_value=100.00, key="Moisture"),
         "Temperature": temperature,
-        "Steam/biomass ratio": steam_biomass.number_input("Steam/biomass ratio (wt/wt)", value=None, min_value=0.00),
+        "Steam/biomass ratio": steam_biomass.number_input("Steam/biomass ratio (wt/wt)", value=None, min_value=0.00, key="Steam/biomass ratio"),
         "ER": equivalence_ratio.number_input("Equivalence ratio of non-steam agent", value=None, min_value=0.00),
     }
+    st.write(st.session_state[continuous_inputs["ER"]])
 
     categorical_col1, categorical_col2 = st.columns(2)
     categorical_inputs = {
@@ -116,11 +117,12 @@ with st.container(border=True):
     y = np.array([H2.item(), CO2.item()])
     diff_H2, diff_CO2 = y - y0
 
-    res1, res2, _, submit = st.columns(4)
+    res1, res2, _, reset = st.columns(4)
     res1.metric("H₂ (vol.% db)", f"{H2.item():.2f}", f"{diff_H2:.2f}")
     res2.metric("CO₂ (vol.% db)", f"{CO2.item():.2f}", f"{diff_CO2:.2f}")
 
     np.savetxt(f"{path}/data/raw/y0.txt", y)
 
-    submit.text("")
-    submit.button("Submit")
+    reset.text("")
+    reset.text("")
+    reset.button("Submit")
