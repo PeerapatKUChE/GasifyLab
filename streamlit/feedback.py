354d364ba6efd1a446a8130c54d6581bac6211d4
peerapatjaiyen <161545115+peerapatjaiyen@users.noreply.github.com>
2024-03-05 20:10:36 +0700

    Update feedback.py

--- streamlit/feedback.py
+++ streamlit/feedback.py
@@ -2,10 +2,12 @@ import streamlit as st
 from datetime import datetime
 
 st.title("Feedback")
+st.write("We appreciate your commitment to improving our platform! Your feedback and bug reports are invaluable in helping us enhance the user experience and address any issues promptly. Please use the form below to share your thoughts or report any bugs you may have encountered.")
 
-# datetime object containing current date and time
+st.text_input("Subject")
+st.text_area("Description")
 now = datetime.now()
- 
+
 st.write("now =", now)
 
 # dd/mm/YY H:M:S
