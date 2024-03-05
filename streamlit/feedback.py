33794948012a63780053957cc180653791b556d9
peerapatjaiyen <161545115+peerapatjaiyen@users.noreply.github.com>
2024-03-05 19:13:46 +0700

    Update feedback.py

--- streamlit/feedback.py
+++ streamlit/feedback.py
@@ -1,10 +1,3 @@
 import streamlit as st
 
 st.title("Feedback")
-
-from trubrics.integrations.streamlit import FeedbackCollector
-
-collector = FeedbackCollector()
-collector.st_feedback(feedback_type="issue")
-
-collector.st_feedback(feedback_type="faces")
