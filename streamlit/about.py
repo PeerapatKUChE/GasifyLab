2a5aa3e973ac3a8544218e38bf5ab8db2e2f954e
peerapatjaiyen <161545115+peerapatjaiyen@users.noreply.github.com>
2024-03-05 15:43:55 +0700

    Update about.py

--- streamlit/about.py
+++ streamlit/about.py
@@ -1,3 +1,14 @@
 import streamlit as st
 
-st.text("TEST")
+st.title("About")
+st.write("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum rutrum pretium leo ac venenatis. Maecenas ipsum odio, vulputate at ante ut, volutpat sodales urna. Nulla at auctor nisl. Donec rutrum augue at ante tincidunt posuere. Praesent molestie fermentum scelerisque. Nulla tristique hendrerit tempus. Aenean eu quam eros. Sed tempor sem hendrerit ipsum ultrices eleifend.")
+
+st.write("Aliquam erat volutpat. Donec blandit commodo lectus nec elementum. Proin gravida feugiat arcu et hendrerit. Fusce suscipit, diam ac tempor ultricies, risus ante gravida justo, nec mollis odio orci vitae nulla. Proin metus erat, ultrices blandit purus ut, pulvinar pulvinar urna. Proin porta, lorem et faucibus faucibus, nulla mauris blandit tortor, id interdum magna neque et est. Maecenas vehicula mauris neque, nec cursus sem blandit et.")
+
+st.write("Mauris tortor nulla, aliquet in feugiat a, mollis in urna. Nam gravida orci nulla, id molestie nunc sagittis ac. Nam non augue sapien. Phasellus est erat, consectetur ut lacus nec, vehicula maximus nisl. Ut porttitor, magna sit amet vehicula auctor, ante ante iaculis sem, eu facilisis elit augue nec elit. Nullam ac sagittis leo, sed mattis arcu. Aenean venenatis posuere lectus, consequat molestie nisi sodales sed. Fusce tempus augue magna, ac sagittis metus vehicula porta. Fusce suscipit purus at ante ultrices elementum. Nunc congue efficitur mi ac ullamcorper. Etiam quis mollis lorem. Nunc finibus, enim venenatis aliquet mollis, augue elit ornare enim, sit amet malesuada risus odio eu quam. Phasellus facilisis fringilla arcu, vitae rutrum odio scelerisque a. Vivamus fringilla maximus risus eget commodo. Aenean nisl tortor, commodo sit amet massa a, efficitur suscipit enim. Phasellus id velit fringilla, vestibulum neque in, tristique mi.")
+
+st.write("Quisque at dignissim dolor, ac tincidunt nisl. Phasellus mollis lorem lacus, sit amet condimentum odio rhoncus id. Sed pulvinar velit sit amet gravida dapibus. Aliquam et egestas urna, id maximus velit. Quisque massa orci, pulvinar et diam vel, congue tincidunt metus. Phasellus a elit porttitor orci volutpat luctus ac sit amet nunc. In mollis ante vel nulla malesuada, sed interdum est dignissim. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed fringilla massa lorem, et luctus neque mollis eget. Nullam suscipit libero vitae nisi pellentesque eleifend.")
+
+st.write("Morbi sagittis augue quam, et feugiat sapien semper id. Sed eleifend velit ut lectus semper, id mollis purus aliquet. Praesent vehicula imperdiet cursus. Cras ultricies, lacus in consequat sollicitudin, lorem neque pulvinar ligula, sit amet vestibulum tortor augue a risus. Suspendisse feugiat ultrices enim sagittis pulvinar. Maecenas faucibus, metus non feugiat dapibus, mauris lacus feugiat dui, id mattis turpis neque vel arcu. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nulla interdum massa in pharetra venenatis.")
+
+st.link_button("Estimation Tool", "https://biomassgaspredict.streamlit.app/Estimation%20Tool")
