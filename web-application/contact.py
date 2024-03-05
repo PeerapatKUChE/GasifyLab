3c31f5d57fe09a4a5053c1ec7191fb3016b4220f
peerapatjaiyen <161545115+peerapatjaiyen@users.noreply.github.com>
2024-03-05 22:00:15 +0700

    Update contact.py

--- web-application/contact.py
+++ web-application/contact.py
@@ -1,3 +1,4 @@
 import streamlit as st
 
 st.title("Contact")
+st.write("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sagittis nisl id vulputate molestie. Curabitur nec faucibus dui. Interdum et.")
