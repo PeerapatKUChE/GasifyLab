6a203c29786393c8f48a52c46ca4850741550bea
peerapatjaiyen <161545115+peerapatjaiyen@users.noreply.github.com>
2024-03-05 21:56:41 +0700

    Update feedback.py

--- web-application/feedback.py
+++ web-application/feedback.py
@@ -11,7 +11,7 @@ feedback = pd.read_csv(os.path.dirname(__file__) + "/feedback.csv").drop(columns
 with st.form("Feedback Form", clear_on_submit=True, border=False):
     subject = st.text_input("Subject *", value=None)
     message = st.text_area("Message *", value=None)
-    attachments = st.text_input("Attachment links (optional)", value=None)
+    attachments = st.text_input("Attachment link(s) :gray[(optional)]", value=None)
 
     if st.form_submit_button("Submit"):
         if subject != None and message != None:
