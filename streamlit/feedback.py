248777d355c05c91b23906fb94111150f61979a7
peerapatjaiyen <161545115+peerapatjaiyen@users.noreply.github.com>
2024-03-05 20:42:15 +0700

    Update feedback.py

--- streamlit/feedback.py
+++ streamlit/feedback.py
@@ -6,7 +6,7 @@ from datetime import datetime
 st.title("Feedback")
 st.write("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur semper pharetra aliquet. In facilisis, velit a molestie sollicitudin, nisl tellus sagittis eros, vel vehicula est elit nec odio. Vivamus luctus, tortor at scelerisque congue, metus neque suscipit lectus, bibendum ultrices nisi mi sed ex. Mauris aliquet eros sit amet pellentesque.")
 
-feedback = pd.read_csv(os.path.dirname(__file__) + "/feedback.csv")
+feedback = pd.read_csv(os.path.dirname(__file__) + "/feedback.csv").drop(columns=["Unnamed: 0"])
 
 with st.form("Feedback Form", clear_on_submit=True, border=False):
     subject = st.text_input("Subject *")
