dcc18063f487be0955cd9b1eacf3e3d29195dd3f
peerapatjaiyen <161545115+peerapatjaiyen@users.noreply.github.com>
2024-03-05 20:59:39 +0700

    Update feedback.py

--- streamlit/feedback.py
+++ streamlit/feedback.py
@@ -7,13 +7,13 @@ st.title("Feedback")
 st.write("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur semper pharetra aliquet. In facilisis, velit a molestie sollicitudin, nisl tellus sagittis eros, vel vehicula est elit nec odio. Vivamus luctus, tortor at scelerisque congue, metus neque suscipit lectus, bibendum ultrices nisi mi sed ex. Mauris aliquet eros sit amet pellentesque.")
 
 feedback = pd.read_csv(os.path.dirname(__file__) + "/feedback.csv").drop(columns=["Unnamed: 0"])
-st.dataframe(feedback)
+
 with st.form("Feedback Form", clear_on_submit=True, border=False):
-    subject = st.text_input("Subject *")
-    message = st.text_area("Message *")
+    subject = st.text_input("Subject *", value=None)
+    message = st.text_area("Message *", value=None)
     attachments = st.text_input("Attachment links (optional)", value=None)
 
-    if st.form_submit_button("Submit"):
+    if st.form_submit_button("Submit") and subject != None and message != None:
         now = datetime.now()
         date = now.strftime("%d/%m/%Y")
         time = now.strftime("%H:%M:%S")
@@ -27,5 +27,11 @@ with st.form("Feedback Form", clear_on_submit=True, border=False):
         }, index=[feedback.shape[0]])
 
         feedback = pd.concat([feedback, latest_feedback])
-        st.dataframe(feedback)
-        feedback.to_csv(os.path.dirname(__file__) + "/feedback.csv")
+
+        feedback.to_csv("feedback.csv")
+    
+    else:
+        if subject is None:
+            st.error("Error: Subject cannot be blank.")
+        if message is None:
+            st.error("Error: Message cannot be blank.")
