d952615ea6f7a845c9167afb946b183ad7a1e49b
peerapatjaiyen <161545115+peerapatjaiyen@users.noreply.github.com>
2024-03-05 19:55:47 +0700

    Update feedback.py

--- streamlit/feedback.py
+++ streamlit/feedback.py
@@ -6,8 +6,8 @@ st.title("Feedback")
 # datetime object containing current date and time
 now = datetime.now()
  
-print("now =", now)
+st.write("now =", now)
 
 # dd/mm/YY H:M:S
 dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
-print("date and time =", dt_string)
+st.write("date and time =", dt_string)
