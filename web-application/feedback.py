ecd824b21a8072a63043c6a3db2d4efeca35a84d
peerapatjaiyen <161545115+peerapatjaiyen@users.noreply.github.com>
2024-03-06 10:05:42 +0700

    Update feedback.py

--- web-application/feedback.py
+++ web-application/feedback.py
@@ -1,14 +1,14 @@
 import os
 import pandas as pd
 import streamlit as st
-
+import validators
 from datetime import datetime
 
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
 
-creds = ServiceAccountCredentials.from_json_keyfile_name(os.path.dirname(__file__) + "/key/biomass-gasification-faec3b292117.json", scope)
+creds = ServiceAccountCredentials.from_json_keyfile_name(os.path.dirname(__file__) + "/biomass-gasification-faec3b292117.json", scope)
 client = gspread.authorize(creds)
  
 sheet = client.open("Web Application").worksheet("Feedback")  
@@ -20,16 +20,13 @@ st.write(":red[* Required]")
 with st.form("Feedback Form", clear_on_submit=True, border=False):
     subject = st.text_input("Subject :red[*]", value=None)
     message = st.text_area("Message :red[*]", value=None)
-    attachments = st.text_input("Attachment link(s) :gray[(optional)]", value=None)
+    attachments = st.file_uploader("Attachment(s) :gray[(if any)]", accept_multiple_files=True)
 
     if st.form_submit_button("Submit"):
         if subject != None and message != None:
             now = datetime.now()
             date = now.strftime("%d/%m/%Y")
             time = now.strftime("%H:%M:%S")
-
-            if attachments is None:
-                attachments = "N/A"
 
             feedback = [date, time, subject, message, attachments]
             sheet.append_row(feedback)
