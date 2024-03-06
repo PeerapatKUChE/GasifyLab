7fd293f3ff89f67162275f4036516753260f6144
peerapatjaiyen <161545115+peerapatjaiyen@users.noreply.github.com>
2024-03-06 13:37:00 +0700

    Update feedback.py

--- web-application/feedback.py
+++ web-application/feedback.py
@@ -33,8 +33,8 @@ with st.form("Feedback Form", clear_on_submit=True, border=False):
                 encoded_content = base64.b64encode(content).decode("utf-8")
                 attachment_data.append({"filename": file.name, "content": encoded_content})
 
-            st.write(attachment_data)
             feedback = [date, time, subject, message, attachment_data]
+            st.write(feedback)
             sheet.append_row(feedback)
 
             st.success("Your feedback has been submitted successfully.")
