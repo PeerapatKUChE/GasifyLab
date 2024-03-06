b28a1b47beb1da1855128f332802e8b7ca3a4d22
peerapatjaiyen <161545115+peerapatjaiyen@users.noreply.github.com>
2024-03-06 19:23:15 +0700

    Update main.py

--- main.py
+++ main.py
@@ -109,7 +109,7 @@ def main():
         "CO2": load(os.path.dirname(__file__) + "/models/model-CO2.joblib")
     }
 
-    with st.container(border=True):
+    with st.form("Estimation Tool"):
         particle_size = st.number_input("Particle size (mm)", value=None, min_value=0.01, key="Particle size")
         carbon, hydrogen = st.columns(2)
         ash, moisture = st.columns(2)
@@ -139,7 +139,7 @@ def main():
 
         submit_button, _, reset_button = st.columns([1, 5, 1])
 
-        if submit_button.button("Estimate", type="secondary"):
+        if submit_button.form_submit_button("Estimate", type="secondary"):
             if not any(value is None for value in categorical_inputs.values()) and not any(value is None for value in continuous_inputs.values()):
                 if validate_inputs(categorical_inputs, continuous_inputs):
                     H2, CO2 = predict_gasification(models, continuous_inputs, categorical_inputs, categorical_vars, continuous_vars, target_data)
@@ -151,12 +151,12 @@ def main():
             for key in list(continuous_inputs.keys()) + list(categorical_inputs.keys()):
                 st.session_state[key] = None
         
-        reset_button.button("**:red[Reset]**", on_click=reset, type="primary")
+        reset_button.form_submit_button("**:red[Reset]**", on_click=reset, type="primary")
 
         st.markdown(
             """
             <style>
-            button[kind="primary"] {
+            form_submit_button[kind="primary"] {
                 background: none!important;
                 border: none;
             }
@@ -168,7 +168,7 @@ def main():
         st.markdown(
             """
             <style>
-            button[kind="secondary"] {
+            form_submit_button[kind="secondary"] {
                 background-color: "ff4b4b";
                 color: white;
             }
