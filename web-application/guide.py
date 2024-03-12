cef812bd824e312e00d197c699ae7a270d23253a
peerapatjaiyen <161545115+peerapatjaiyen@users.noreply.github.com>
2024-03-12 16:35:49 +0700

    Update guide.py

--- web-application/guide.py
+++ web-application/guide.py
@@ -2,6 +2,9 @@ import streamlit as st
 
 st.title("User Guide")
 st.subheader("Getting Started")
-st.write("1. Accessing the Web App:")
 
-st.write("‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ Open your preferred web browser and navigate to [the estimation tool](https://gasifylab.streamlit.app/Estimation%20Tool).")
+number, text = st.columns([1, 10]
+number.write("1.")
+text.write("Accessing the Web App:")
+
+text.write("Open your preferred web browser and navigate to [the estimation tool](https://gasifylab.streamlit.app/Estimation%20Tool).")
