f17c355e2a9da3c8ac9edf2c9f901ab707ede83f
peerapatjaiyen <161545115+peerapatjaiyen@users.noreply.github.com>
2024-03-12 16:36:04 +0700

    Update guide.py

--- web-application/guide.py
+++ web-application/guide.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.title("User Guide")
 st.subheader("Getting Started")
 
-number, text = st.columns([1, 10]
+number, text = st.columns([1, 10])
 number.write("1.")
 text.write("Accessing the Web App:")
 
