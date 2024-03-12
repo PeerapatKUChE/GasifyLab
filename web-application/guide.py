46e99b7cf2f1bcca4c4c981b8645bc3fad89fe2c
peerapatjaiyen <161545115+peerapatjaiyen@users.noreply.github.com>
2024-03-12 16:40:25 +0700

    Update guide.py

--- web-application/guide.py
+++ web-application/guide.py
@@ -10,3 +10,9 @@ number.write("‎ ")
 text.write("Open your preferred web browser and navigate to [the estimation tool](https://gasifylab.streamlit.app/Estimation%20Tool).")
 number.write("2.‎ ")
 
+st.text_area("Access the Web App:
+Open your web browser and go to www.biomass-estimator.com.
+
+Homepage:
+You will be directed to the homepage where you can find a brief introduction to the estimator. Click on the "Get Started" button to proceed.")
+
