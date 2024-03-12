eb1318ad94d3d2f0ffe62b3f41678f453faf9538
peerapatjaiyen <161545115+peerapatjaiyen@users.noreply.github.com>
2024-03-12 16:38:47 +0700

    Update guide.py

--- web-application/guide.py
+++ web-application/guide.py
@@ -6,5 +6,7 @@ st.subheader("Getting Started")
 number, text = st.columns([1, 10])
 number.write("1.‎ ")
 text.write("Accessing the Web App:")
-
+number.write("‎ ")
 text.write("Open your preferred web browser and navigate to [the estimation tool](https://gasifylab.streamlit.app/Estimation%20Tool).")
+number.write("2.‎ ")
+
