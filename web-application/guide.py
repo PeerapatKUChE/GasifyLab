69d639d9e1e43bf0a5ffcf3d6274772dc2c53bfc
peerapatjaiyen <161545115+peerapatjaiyen@users.noreply.github.com>
2024-03-12 16:25:42 +0700

    Update guide.py

--- web-application/guide.py
+++ web-application/guide.py
@@ -4,4 +4,4 @@ st.title("User Guide")
 st.subheader("1. Getting Started")
 st.write("* Accessing the Web App:")
 
-st.write("  Open your preferred web browser and navigate to [the estimation tool](https://gasifylab.streamlit.app/Estimation%20Tool).")
+st.write(" " * 7 + "Open your preferred web browser and navigate to [the estimation tool](https://gasifylab.streamlit.app/Estimation%20Tool).")
