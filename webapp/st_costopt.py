74cadcca1af13467cc79f222618d7e79b1136e79
peerapatjaiyen <161545115+peerapatjaiyen@users.noreply.github.com>
2024-06-04 17:55:04 +0700

    Update st_costopt.py

--- webapp/st_costopt.py
+++ webapp/st_costopt.py
@@ -10,13 +10,83 @@ def load_data(path):
     distances = pd.read_excel(path+"data/raw/Data-Distances.xlsx")
     return compositions, supplies, distances
 
-def prepare_data():
-    return
+def calculate_transportation_cost(
+        fuel_price, fuel_consumption_rate, maintenance_cost, tire_price,
+        tire_lifespan, number_of_tires, cargo_width, cargo_length, cargo_height,
+        cargo_capacity, densities
+):
+    fuel_consumption_cost = fuel_price / fuel_consumption_rate
+    average_tire_cost_per_km = tire_price * number_of_tires / tire_lifespan
+    total_variable_cost = fuel_consumption_cost + average_tire_cost_per_km + maintenance_cost
+    cargo_volume = cargo_width * cargo_length * cargo_height
 
-def calculate_transportation_cost():
-    return
+    transportation_costs_df = pd.DataFrame()
+    for i in range(densities.shape[0]):
+        weight_at_max_volume = densities.iloc[i] * cargo_volume / 1000
+        weight_at_max_capacity = min(weight_at_max_volume, cargo_capacity)
+        transportation_cost = total_variable_cost / weight_at_max_capacity
+        transportation_cost = pd.DataFrame([transportation_cost], index=[i])
+        transportation_costs_df = pd.concat([transportation_costs_df, transportation_cost])
 
-def milp_solver():
+    return transportation_costs_df
+
+def prepare_data(
+        target_composition, compositions, prices, supplies, distances,
+        fuel_price, fuel_consumption_rate, maintenance_cost, tire_price,
+        tire_lifespan, number_of_tires, cargo_width, cargo_length, cargo_height, cargo_capacity
+    ):
+    biomass_data = compositions.merge(prices, on="Biomass Type")
+    biomass_data = biomass_data.drop(columns=["Feedstock Cost Reference", "Density Reference", "Weight at Max Cargo Capacity", "Transportation Cost"])
+    biomass_data["Transportation Cost"] = calculate_transportation_cost(
+        fuel_price, fuel_consumption_rate, maintenance_cost, tire_price,
+        tire_lifespan, number_of_tires, cargo_width, cargo_length, cargo_height,
+        cargo_capacity, biomass_data["Density"]
+    )
+    biomass_data = biomass_data.drop(columns=["Density"])
+    biomass_data = biomass_data.sort_values(by=["Biomass Type"])
+
+    st.dataframe(biomass_data)
+
+    supplies = supplies.drop(columns=["No.", "Region"])
+    S = supplies.T
+    S.columns = supplies["Province"]
+    S = S.sort_index(axis=0)
+    S = S.sort_index(axis=1)
+    S = S.drop(["Province"])
+    S.columns.names = [""]
+
+    distances = distances.drop(columns=["Latitude", "Longitude"])
+    D = distances.drop(columns=["Plant Code"])
+    D.index = distances["Plant Code"]
+    D = D.sort_index(axis=0)
+    D = D.sort_index(axis=1)
+    D.index.names = [""]
+
+    Nb = S.shape[0]
+    Ns = D.shape[1]
+    Ng = D.shape[0]
+    C = biomass_data["C"]
+    H = biomass_data["H"]
+    A = biomass_data["Ash"]
+    Ct = target_composition["Target carbon"]
+    Ht = target_composition["Target hydrogen"]
+    At = target_composition["Target ash"]
+    F = biomass_data["Feedstock Cost"]
+    T = biomass_data["Transportation Cost"]
+
+    return Nb, Ns, Ng, C, H, A, Ct, Ht, At, F, T, D, S
+
+def milp_solver(
+        target_composition, compositions, prices, supplies, distances,
+        fuel_price, fuel_consumption_rate, maintenance_cost, tire_price,
+        tire_lifespan, number_of_tires, cargo_width, cargo_length, cargo_height, cargo_capacity
+    ):
+
+    Nb, Ns, Ng, C, H, A, Ct, Ht, At, F, T, D, S = prepare_data(
+        target_composition, compositions, prices, supplies, distances,
+        fuel_price, fuel_consumption_rate, maintenance_cost, tire_price,
+        tire_lifespan, number_of_tires, cargo_width, cargo_length, cargo_height, cargo_capacity
+        )
     return
 
 def main():
@@ -37,7 +107,7 @@ def main():
             1800.00, 5000.00, 1000.00, 1500.00, 500.00, 50.00, 500.00,
             3200.00, 500.00, 1500.00, 2000.00, 600.00, 500.00, 800.00
         ]
-        biomass_price = {
+        biomass_prices = {
             "Biomass Type": [
                 "Cassava rhizome", "Coconut coir", "Coconut shell", "Corn stalk", "Corncob",
                 "Palm empty fruit bunch", "Palm frond", "Palm kernel shell", "Palm trunk", "Rice husk",
@@ -46,13 +116,13 @@ def main():
             "Price (THB/ton)": default_biomass_price
         }
 
-        if 'biomass_price' not in st.session_state:
-            biomass_price = pd.DataFrame(biomass_price)
-            st.session_state.biomass_price = biomass_price
+        if "biomass_prices" not in st.session_state:
+            biomass_prices = pd.DataFrame(biomass_prices)
+            st.session_state.biomass_prices = biomass_prices
             st.session_state.key = 0
-        biomass_price = st.session_state.biomass_price
+        biomass_prices = st.session_state.biomass_prices
 
-        biomass_price = col1.data_editor(biomass_price, disabled=["Biomass Type"], hide_index=True, key=f"Biomass price edited #{st.session_state.key}")
+        biomass_prices = col1.data_editor(biomass_prices, disabled=["Biomass Type"], hide_index=True, key=f"Biomass price edited #{st.session_state.key}")
 
         col2.write("**Truck Operational Parameters**")
         col3.write("‎ ")
@@ -86,7 +156,25 @@ def main():
         submit_button, _, reset_button = st.columns([1.2, 4.9, 1])
 
         if submit_button.form_submit_button("**Submit**", type="primary"):
-            st.dataframe(biomass_price)
+            compositions, supplies, distances = load_data(os.path.abspath(os.curdir))
+
+            prepare_data(
+                target_composition=target_composition,
+                compositions=compositions,
+                prices=biomass_prices,
+                supplies=supplies,
+                distances=distances,
+                fuel_price=truck_params["Fuel price"],
+                fuel_consumption_rate=truck_params["Fuel consumption rate"],
+                maintenance_cost=truck_params["Maintenance cost"],
+                tire_price=truck_params["Tire price"],
+                tire_lifespan=truck_params["Tire lifespan"],
+                number_of_tires=truck_params["Number of tires"],
+                cargo_width=truck_params["Cargo width"],
+                cargo_length=truck_params["Cargo length"],
+                cargo_height=truck_params["Cargo height"],
+                cargo_capacity=truck_params["Cargo capacity"]
+            )
         
         def reset():
             for target_key in list(target_composition.keys()):
