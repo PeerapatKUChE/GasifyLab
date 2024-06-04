a995176e17f4ffe33e2381192f58d192105cb4ea
peerapatjaiyen <161545115+peerapatjaiyen@users.noreply.github.com>
2024-06-04 18:57:02 +0700

    Update st_costopt.py

--- webapp/st_costopt.py
+++ webapp/st_costopt.py
@@ -73,7 +73,7 @@ def prepare_data(
     Ct = target_composition["Target carbon"]
     Ht = target_composition["Target hydrogen"]
     At = target_composition["Target ash"]
-    F = prices
+    F = prices["Price (THB/ton)"]
     T = biomass_data["Transportation Cost"]
 
     return Nb, Ns, Ng, C, H, A, Ct, Ht, At, F, T, D, S
@@ -90,9 +90,183 @@ def milp_solver(
         tire_lifespan, number_of_tires, cargo_width, cargo_length, cargo_height, cargo_capacity
         )
     
-    st.write(Nb, Ns, Ng, C, H, A, Ct, Ht, At)
-    st.dataframe(F)
-    st.dataframe(T)
+        #
+    prob = pulp.LpProblem('Cost Optimization', pulp.LpMinimize)
+
+    # Decision variables ===============================================================================
+    #
+    X = np.array([
+        pulp.LpVariable(f'X_{j}_{k}_{l}', lowBound=0)
+        for j in range(Nb)
+        for k in range(Ns)
+        for l in range(Ng)
+    ]).reshape(Nb, Ns, Ng)
+
+    #
+    Y = np.array([
+        pulp.LpVariable(f'Y_{l}_{k}', cat='Binary')
+        for l in range(Ng)
+        for k in range(Ns)
+    ]).reshape(Ng, Ns)
+
+    #
+    Ys = np.array([pulp.LpVariable(f'Yp_{k}', cat='Binary') for k in range(Ns)]).reshape(1, Ns)
+
+    #
+    Yg = np.array([pulp.LpVariable(f'Yg_{l}', cat='Binary') for l in range(Ng)]).reshape(Ng, 1)
+
+    # Objective function ===============================================================================
+    #
+    FC = pulp.lpSum(X * F.values.reshape(Nb, 1, 1))
+
+    #
+    TC = pulp.lpSum(np.sum(X * D.values.T.reshape(1, Ns, Ng), axis=2) * T.values.reshape(Nb, 1))
+
+    #
+    prob += FC + TC
+
+    # Constraints ======================================================================================
+    # 1.
+    prob += pulp.lpSum(Yg) == 1
+
+    # 2.
+    for k in range(Ns):
+        prob += pulp.lpSum(Y[:, k]) == Ys[0, k]
+
+    # 3.
+    for l in range(Ng):
+        prob += pulp.lpSum(Y[l, :]) <= Yg[l, 0] * Ns
+
+    # 4.
+    for j in range(Nb):
+        for k in range(Ns):
+            prob += pulp.lpSum(X[j, k, :]) <= S.iloc[j, k]
+
+    # 5.
+    M = 10**15
+    for k in range(Ns):
+        for l in range(Ng):
+            prob += pulp.lpSum(X[:, k, l]) <= Y[l, k] * M
+
+    # 6.
+    prob += Ct * pulp.lpSum(X) == pulp.lpDot(C.values, [pulp.lpSum(X[j, :, :]) for j in range(Nb)])
+
+    # 7.
+    prob += Ht * pulp.lpSum(X) == pulp.lpDot(H.values, [pulp.lpSum(X[j, :, :]) for j in range(Nb)])
+
+    # 8.
+    prob += pulp.lpSum(X) >= 10000
+
+    # 9.
+    prob += pulp.lpSum(Y) >= 1
+    
+    # Solve the problem
+    status = prob.solve()
+
+    # Result Analysis ==================================================================================
+    #
+    if status == pulp.LpStatusOptimal:
+        #
+        details = pd.DataFrame()
+        summary = pd.DataFrame()
+
+        #
+        Yg_val = []
+        for l in range(Ng):
+            Yg_val.append(Yg[l, 0].value())
+        Yg_val = np.array(Yg_val).reshape(Ng, 1)
+        Yg_val = pd.DataFrame(Yg_val, index=distances['Plant Code'])
+        plant = Yg_val[Yg_val==1].dropna()
+
+        #
+        X_val = []
+        for j in range(Nb):
+            for k in range(Ns):
+                for l in range(Ng):
+                    X_val.append(X[j, k, l].value())
+        X_val = np.array(X_val).reshape(Nb, Ns, Ng)
+        X_val = np.sum(X_val, axis=2)
+        X_val = pd.DataFrame(X_val, columns=D.columns, index=S.index)
+
+        #
+        supplier_indices = X_val.any(axis=0)
+        supply = X_val.loc[:, supplier_indices].T
+
+        #
+        composition_i = np.zeros(supply.shape[0]).reshape(supply.shape[0], 1)
+        composition_i = pd.DataFrame(
+            composition_i,
+            columns=['Composition #i'],
+            index=range(details.shape[0], details.shape[0]+supply.shape[0])
+        )
+        details = pd.concat([details, composition_i.astype(int)], axis=0)
+
+        #
+        supplier = pd.DataFrame(supply.index, columns=['Province'], index=composition_i.index)
+        details = pd.concat([details, supplier], axis=1)
+
+        #
+        distance = D.loc[plant.index, supply.index].T
+        distance.index = composition_i.index
+        distance.columns = ['Distance']
+        details = pd.concat([details, distance], axis=0)
+
+        #
+        supply.index = composition_i.index
+        details = pd.concat([details, supply], axis=1)
+
+        #
+        result_details = pd.concat([result_details, details], axis=0)
+
+        #
+        feedstock_cost = FC.value()
+        transport_cost = TC.value()
+        total_cost = feedstock_cost + transport_cost
+
+        #
+        cost = pd.DataFrame(
+            np.array([plant.index.values[0], total_cost, feedstock_cost, transport_cost]).reshape(1, 5),
+            columns=['Selected Plant Code', 'Total Cost', 'Feedstock Cost', 'Transportation Cost'],
+            index=[0]
+        )
+        summary = pd.concat([summary, cost], axis=0)
+
+        #
+        total_distance = distance.sum()
+        total_distance.index = [0]
+        total_distance = pd.DataFrame(total_distance, columns=['Total Distance'])
+        summary = pd.concat([summary, total_distance], axis=1)
+
+        #
+        mixed_carbon = sum([C.values[j] * X_val.iloc[j, :].sum() for j in range(Nb)]) / X_val.sum().sum()
+        mixed_hydrogen = sum([H.values[j] * X_val.iloc[j, :].sum() for j in range(Nb)]) / X_val.sum().sum()
+        mixed_ash = sum([A.values[j] * X_val.iloc[j, :].sum() for j in range(Nb)]) / X_val.sum().sum()
+
+        #
+        composition = pd.DataFrame(
+            np.array([mixed_carbon, mixed_hydrogen, mixed_ash, At]).reshape(1, 4),
+            columns=['Mixed Carbon', 'Mixed Hydrogen', 'Mixed Ash', 'Target Ash'],
+            index=[0]
+        )
+        summary = pd.concat([summary, composition], axis=1)
+
+        #
+        total_supply = X_val.T.sum().sum()
+        total_supply = pd.DataFrame([total_supply], columns=['Total Supply'], index=[0])
+        summary = pd.concat([summary, total_supply], axis=1)
+
+        #
+        biomass_percentage = X_val.T.sum() / total_supply.values[0][0] * 100
+        biomass_percentage = pd.DataFrame(biomass_percentage).T
+        biomass_percentage.index = [0]
+        summary = pd.concat([summary, biomass_percentage], axis=1)
+
+        #
+        result_summary = pd.concat([result_summary, summary], axis=0)
+
+    else:
+        print(f'Error: No solution found for this composition.')
+
     return
 
 def main():
