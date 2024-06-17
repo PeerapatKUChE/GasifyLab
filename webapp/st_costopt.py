f24f07f7750c74e79eae7ac11c78425c8ec1b00d
peerapatjaiyen <161545115+peerapatjaiyen@users.noreply.github.com>
2024-06-17 16:40:48 +0700

    Update st_costopt.py

--- webapp/st_costopt.py
+++ webapp/st_costopt.py
@@ -5,17 +5,6 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-default_summmary = {
-    "Selected Plant Code": "-",
-    "Total Cost (THB/year)": "0.00",
-    "Feedstock Cost (THB/year)": "0.00",
-    "Transportation Cost (THB/year)": "0.00",
-    "Total Distance (km)": "0.00",
-    "Total Supply (tons)": "0.00"
-}
-
-default_selected_feedstock = pd.DataFrame(np.ones(1).reshape(1, 1), index=[0], columns=["No Data"])
-
 def load_data(path):
     compositions = pd.read_excel(path+"/data/raw/Data-ThaiBiomassComposition.xlsx", sheet_name="Processed Data")
     densities = pd.read_excel(path+"/data/raw/Data-ThaiBiomass.xlsx", sheet_name="Biomass Cost")
@@ -92,7 +81,7 @@ def milp_solver(
         prices, target_composition, compositions, densities, supplies, distances,
         fuel_price, fuel_consumption_rate, maintenance_cost, tire_price,
         tire_lifespan, number_of_tires, cargo_width, cargo_length, cargo_height, cargo_capacity,
-        min_supply
+        min_supply, default_summmary, default_selected_feedstock
     ):
 
     Nb, Ns, Ng, C, H, Ct, Ht, F, T, D, S = prepare_data(
@@ -255,6 +244,263 @@ def main():
 
     compositions, densities, supplies, distances = load_data(os.path.abspath(os.curdir))
 
+    import os
+import pulp
+import streamlit as st
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def load_data(path):
+    compositions = pd.read_excel(path+"/data/raw/Data-ThaiBiomassComposition.xlsx", sheet_name="Processed Data")
+    densities = pd.read_excel(path+"/data/raw/Data-ThaiBiomass.xlsx", sheet_name="Biomass Cost")
+    supplies = pd.read_excel(path+"/data/raw/Data-ThaiBiomass.xlsx", sheet_name="Biomass Data")
+    distances = pd.read_excel(path+"/data/raw/Data-Distances.xlsx")
+    return compositions, densities, supplies, distances
+
+def calculate_transportation_cost(
+        fuel_price, fuel_consumption_rate, maintenance_cost, tire_price,
+        tire_lifespan, number_of_tires, cargo_width, cargo_length, cargo_height,
+        cargo_capacity, densities
+):
+    fuel_consumption_cost = fuel_price / fuel_consumption_rate
+    average_tire_cost_per_km = tire_price * number_of_tires / tire_lifespan
+    total_variable_cost = fuel_consumption_cost + average_tire_cost_per_km + maintenance_cost
+    cargo_volume = cargo_width * cargo_length * cargo_height
+
+    transportation_costs_df = pd.DataFrame()
+    for i in range(densities.shape[0]):
+        weight_at_max_volume = densities.iloc[i] * cargo_volume / 1000
+        weight_at_max_capacity = min(weight_at_max_volume, cargo_capacity)
+        transportation_cost = total_variable_cost / weight_at_max_capacity
+        transportation_cost = pd.DataFrame([transportation_cost], index=[i])
+        transportation_costs_df = pd.concat([transportation_costs_df, transportation_cost])
+
+    return transportation_costs_df
+
+def prepare_data(
+        prices, target_composition, compositions, densities, supplies, distances,
+        fuel_price, fuel_consumption_rate, maintenance_cost, tire_price,
+        tire_lifespan, number_of_tires, cargo_width, cargo_length, cargo_height, cargo_capacity
+    ):
+    
+    prices["Biomass Type"] = prices["Biomass Type"].str.lower()
+    biomass_data = compositions.merge(prices, on="Biomass Type")
+    biomass_data = biomass_data.merge(densities[["Biomass Type", "Density"]], on="Biomass Type")
+
+    biomass_data["Transportation Cost"] = calculate_transportation_cost(
+        fuel_price, fuel_consumption_rate, maintenance_cost, tire_price,
+        tire_lifespan, number_of_tires, cargo_width, cargo_length, cargo_height,
+        cargo_capacity, biomass_data["Density"]
+    )
+    biomass_data = biomass_data.drop(columns=["Density"])
+    biomass_data = biomass_data.sort_values(by=["Biomass Type"])
+
+    supplies = supplies.drop(columns=["No.", "Region"])
+    S = supplies.T
+    S.columns = supplies["Province"]
+    S = S.sort_index(axis=0)
+    S = S.sort_index(axis=1)
+    S = S.drop(["Province"])
+    S.columns.names = [""]
+
+    distances = distances.drop(columns=["Latitude", "Longitude"])
+    D = distances.drop(columns=["Plant Code"])
+    D.index = distances["Plant Code"]
+    D = D.sort_index(axis=0)
+    D = D.sort_index(axis=1)
+    D.index.names = [""]
+
+    Nb = S.shape[0]
+    Ns = D.shape[1]
+    Ng = D.shape[0]
+    C = biomass_data["C"]
+    H = biomass_data["H"]
+    Ct = target_composition["Target carbon"]
+    Ht = target_composition["Target hydrogen"]
+    F = prices["Price (THB/ton)"]
+    T = biomass_data["Transportation Cost"]
+
+    return Nb, Ns, Ng, C, H, Ct, Ht, F, T, D, S
+
+def milp_solver(
+        prices, target_composition, compositions, densities, supplies, distances,
+        fuel_price, fuel_consumption_rate, maintenance_cost, tire_price,
+        tire_lifespan, number_of_tires, cargo_width, cargo_length, cargo_height, cargo_capacity,
+        min_supply, default_summmary, default_selected_feedstock
+    ):
+
+    Nb, Ns, Ng, C, H, Ct, Ht, F, T, D, S = prepare_data(
+        prices, target_composition, compositions, densities, supplies, distances,
+        fuel_price, fuel_consumption_rate, maintenance_cost, tire_price,
+        tire_lifespan, number_of_tires, cargo_width, cargo_length, cargo_height, cargo_capacity
+        )
+    
+    st.write("The code is running. Your results will be available within 30 minutes.")
+
+    #
+    prob = pulp.LpProblem("Cost_Optimization", pulp.LpMinimize)
+
+    # Decision variables ===============================================================================
+    #
+    X = np.array([
+        pulp.LpVariable(f"X_{j}_{k}_{l}", lowBound=0)
+        for j in range(Nb)
+        for k in range(Ns)
+        for l in range(Ng)
+    ]).reshape(Nb, Ns, Ng)
+
+    #
+    Y = np.array([
+        pulp.LpVariable(f"Y_{l}_{k}", cat="Binary")
+        for l in range(Ng)
+        for k in range(Ns)
+    ]).reshape(Ng, Ns)
+
+    #
+    Ys = np.array([pulp.LpVariable(f"Yp_{k}", cat="Binary") for k in range(Ns)]).reshape(1, Ns)
+
+    #
+    Yg = np.array([pulp.LpVariable(f"Yg_{l}", cat="Binary") for l in range(Ng)]).reshape(Ng, 1)
+
+    # Objective function ===============================================================================
+    #
+    FC = pulp.lpSum(X * F.values.reshape(Nb, 1, 1))
+
+    #
+    TC = pulp.lpSum(np.sum(X * D.values.T.reshape(1, Ns, Ng), axis=2) * T.values.reshape(Nb, 1))
+
+    #
+    prob += FC + TC
+
+    # Constraints ======================================================================================
+    # 1.
+    prob += pulp.lpSum(Yg) == 1
+
+    # 2.
+    for k in range(Ns):
+        prob += pulp.lpSum(Y[:, k]) == Ys[0, k]
+
+    # 3.
+    for l in range(Ng):
+        prob += pulp.lpSum(Y[l, :]) <= Yg[l, 0] * Ns
+
+    # 4.
+    for j in range(Nb):
+        for k in range(Ns):
+            prob += pulp.lpSum(X[j, k, :]) <= S.iloc[j, k]
+
+    # 5.
+    M = 10**15
+    for k in range(Ns):
+        for l in range(Ng):
+            prob += pulp.lpSum(X[:, k, l]) <= Y[l, k] * M
+
+    # 6.
+    prob += Ct * pulp.lpSum(X) == pulp.lpDot(C.values, [pulp.lpSum(X[j, :, :]) for j in range(Nb)])
+
+    # 7.
+    prob += Ht * pulp.lpSum(X) == pulp.lpDot(H.values, [pulp.lpSum(X[j, :, :]) for j in range(Nb)])
+
+    # 8.
+    prob += pulp.lpSum(X) >= min_supply
+
+    # 9.
+    prob += pulp.lpSum(Y) >= 1
+    
+    # Solve the problem
+    status = prob.solve()
+
+    # Result Analysis ==================================================================================
+    #
+    if status == pulp.LpStatusOptimal:
+        #
+        details = pd.DataFrame()
+
+        #
+        Yg_val = []
+        for l in range(Ng):
+            Yg_val.append(Yg[l, 0].value())
+        Yg_val = np.array(Yg_val).reshape(Ng, 1)
+        Yg_val = pd.DataFrame(Yg_val, index=distances["Plant Code"])
+        plant = Yg_val[Yg_val==1].dropna()
+
+        #
+        X_val = []
+        for j in range(Nb):
+            for k in range(Ns):
+                for l in range(Ng):
+                    X_val.append(X[j, k, l].value())
+        X_val = np.array(X_val).reshape(Nb, Ns, Ng)
+        X_val = np.sum(X_val, axis=2)
+        X_val = pd.DataFrame(X_val, columns=D.columns, index=S.index)
+
+        #
+        supplier_indices = X_val.any(axis=0)
+        supply = X_val.loc[:, supplier_indices].T
+        supplier = pd.DataFrame(supply.index, columns=["Province"], index=range(supply.shape[0]))
+        details = pd.concat([details, supplier], axis=0)
+
+        #
+        distance = D.loc[plant.index, supply.index].T
+        distance.index = range(supply.shape[0])
+        distance.columns = ["Distance (km)"]
+        details = pd.concat([details, distance], axis=1)
+
+        #
+        #
+        supply = supply.loc[:, (supply != 0).any(axis=0)]
+        supply.index = range(supply.shape[0])
+        supply.rename(columns=lambda x: x.capitalize()+" supply (ton/year)", inplace=True)
+        details = pd.concat([details, supply], axis=1)
+
+        #
+        selected_plant_code = plant.index.values[0]
+
+        feedstock_cost = FC.value()
+        transport_cost = TC.value()
+        total_cost = feedstock_cost + transport_cost
+
+        total_distance = distance.sum().values[0]
+
+        total_supply = X_val.T.sum().sum()
+
+        biomass_percentage = X_val.T.sum() / total_supply * 100
+        selected_feedstock = biomass_percentage[biomass_percentage>0]
+        selected_feedstock = pd.DataFrame(selected_feedstock).T
+
+        summary = {
+            "Selected Plant Code": selected_plant_code,
+            "Total Cost (THB/year)": f"{total_cost:,.2f}",
+            "Feedstock Cost (THB/year)": f"{feedstock_cost:,.2f}",
+            "Transportation Cost (THB/year)": f"{transport_cost:,.2f}",
+            "Total Distance (km)": f"{total_distance:,.2f}",
+            "Total Supply (tons)": f"{total_supply:,.2f}"
+        }
+
+    else:
+        summary = default_summmary
+        selected_feedstock = default_selected_feedstock
+        details = None
+    
+    return summary, selected_feedstock, details
+
+def main():
+    st.set_page_config(layout="wide")
+
+    compositions, densities, supplies, distances = load_data(os.path.abspath(os.curdir))
+
+    default_summmary = {
+        "Selected Plant Code": "-",
+        "Total Cost (THB/year)": "0.00",
+        "Feedstock Cost (THB/year)": "0.00",
+        "Transportation Cost (THB/year)": "0.00",
+        "Total Distance (km)": "0.00",
+        "Total Supply (tons)": "0.00"
+    }
+
+    default_selected_feedstock = pd.DataFrame(np.ones(1).reshape(1, 1), index=[0], columns=["No Data"])
+
     page_column1, _, page_column2 = st.columns([0.40, 0.05, 0.55])
     with page_column2.form("Optimization Tool"):
         st.write(":red[* Required]")
@@ -322,6 +568,17 @@ def main():
             "Cargo capacity": col3.number_input("Cargo capacity (ton)", value=default_truck_params["Cargo capacity"], min_value=0.01, key="Cargo capacity"),
         }
 
+        default_summmary = {
+            "Selected Plant Code": "-",
+            "Total Cost (THB/year)": "0.00",
+            "Feedstock Cost (THB/year)": "0.00",
+            "Transportation Cost (THB/year)": "0.00",
+            "Total Distance (km)": "0.00",
+            "Total Supply (tons)": "0.00"
+        }
+
+        default_selected_feedstock = pd.DataFrame(np.ones(1).reshape(1, 1), index=[0], columns=["No Data"])
+
         submit_button, _, reset_button = st.columns([1.2, 4.9, 1])
 
         run_count = 0
@@ -345,7 +602,9 @@ def main():
                     cargo_length=truck_params["Cargo length"],
                     cargo_height=truck_params["Cargo height"],
                     cargo_capacity=truck_params["Cargo capacity"],
-                    min_supply=min_supply
+                    min_supply=min_supply,
+                    default_summmary=default_summmary,
+                    default_selected_feedstock=default_selected_feedstock
                 )
 
             else:
